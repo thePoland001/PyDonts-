6e8417ddaff8a0996dd043cd19439855e58858a2
thePoland001 <thepoland001@gmail.com>
2024-07-25 11:43:20 -0500

    Update enumerate_me.py

--- enumerate_me.py
+++ enumerate_me.py
@@ -1,2 +1,11 @@
-# With enumeration 
+# Without enumeration 
+words = ("You", "Love", "Yourself")
 
+for i in range(len(words)): 
+  print(f"{words[i]} has {len(words[i])} letters")
+
+# With enumeration 
+words = ("You", "Love", "Yourself")
+
+for word in words:
+  print(f"{word} has {len(word)} letters")
